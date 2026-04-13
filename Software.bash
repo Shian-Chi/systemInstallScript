98743ce06c3cce2cde241051ab9d4332bb10026d
Shian-Chi <109353604+Shian-Chi@users.noreply.github.com>
2026-04-13 16:36:02 +0800

    Refactor Software.bash for tool installation and cleanup
    Updated installation script to use curl for downloading VS Code and added cleanup step.

--- Software.bash
+++ Software.bash
@@ -1,6 +1,15 @@
-sudo apt install vlc wget
+# 安裝基礎工具
+sudo apt update
+sudo apt install -y vlc wget curl
 
-wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 -O vscode.deb
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
+# 下載 VS Code (加上 -L 處理重導向)
+curl -L "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -o vscode.deb
 
-sudo dpkg -i vscode.deb chrome.deb
+# 下載 Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o chrome.deb
+
+# 使用 apt 安裝本地 deb (會自動處理依賴問題)
+sudo apt install -y ./vscode.deb ./chrome.deb
+
+# 清理下載的安裝包
+rm vscode.deb chrome.deb
